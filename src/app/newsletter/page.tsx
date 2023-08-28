bd48b0046d4a2afddbf833859cedd46d95657c01
Marco Greiveldinger <43136849+marcogreiveldinger@users.noreply.github.com>
2023-08-28 17:43:21 +1200

    remove dash between privacy policy

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
                             href="/privacy-policy"
                             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                         >
-                            Privacy-policy
+                            Privacy policy
                         </Link>
                     </p>
                 </div>
